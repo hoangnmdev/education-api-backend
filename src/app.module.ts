bbbf0e11c0661eafb4da045f3debd535098300e5
hoangnmdev <hoangnm06122003@gmail.com>
2025-03-20 02:16:04 +0700

    load_app_config_ignore_test_migration

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,16 @@
 import { Module } from '@nestjs/common';
 import { TeacherStudentModule } from './teachers/teachers.module';
 import { SeedModule } from './database/seeds/document/seed.module';
+import appConfig from './config/app.config';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [TeacherStudentModule, SeedModule],
+  imports: [
+    ConfigModule.forRoot({
+      load: [appConfig],
+    }),
+    TeacherStudentModule,
+    SeedModule,
+  ],
 })
 export class AppModule {}
